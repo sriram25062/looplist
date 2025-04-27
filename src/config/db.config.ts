3772ce44a88fbcc9c15f928b5cf46840035cffe8
Sriram R <126612224+sriram25062@users.noreply.github.com>
2025-04-27 13:28:41 +0530

    changs

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -7,4 +7,7 @@ export const dbConfig = {
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
+    ssl: {
+        rejectUnauthorized: true, // Ensures SSL connections are validated
+    },
 }
